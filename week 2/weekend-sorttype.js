8223a50269d268e923efc3e366609a00268c1168
MizushimaShiba <haidarme@yahoo.co.id>
2019-12-07 19:41:10 +0700

    Cimmoet

--- week 2/weekend-sorttype.js
+++ week 2/weekend-sorttype.js
@@ -17,13 +17,115 @@ function sorting(array) {
   return array
 }
 
-console.log(sorting([ 2, 4, 6, 8, 2, 3 ])); //[ 2, 2, 3, 4, 6, 8 ]
+// console.log(sorting([ 2, 4, 6, 8, 2, 3 ])); //[ 2, 2, 3, 4, 6, 8 ]
 
 
 function sortingByType(array) {
   // your code here
+
+  let number = []
+  let bool = []
+  let string = []
+
+  let result = []
   
+  for (let i = 0; i < array.length; i++)
+  {
+    if (typeof array[i] === 'number')
+    {
+      number.push(array[i])
+    } 
+    else if (typeof array[i] === 'string')
+    {
+      string.push(array[i])
+    }
+    else if (typeof array[i] === 'boolean')
+    {
+      bool.push(array[i])
+    } 
+  }
+
+  result.push(number)
+  result.push(string)
+  result.push(bool)
+
+  result[0].sort()
+  result[2].sort()
+
+  return result
 }
 
-console.log(sortingByType([ 1, 3, 'array', -45, true, false, 'big' ]));
+// console.log(sortingByType([ 1, 3, 'array', -45, true, false, 'big' ]));
 // [ [ -45, 1, 3 ], [ 'array', 'big' ], [ false, true ] ]
+
+function sortAllClean(array) {
+  //your code here
+  let result = []
+  let reee = []
+
+  for (let i = 0; i < array.length; i++)
+  {
+    if (array[i] === null || array[i] === undefined  ||typeof array[i] === 'object')
+    {
+      reee.push(array[i])
+    }
+    else if (isNaN(array[i]) === true)
+    {
+      if (typeof array[i] === 'string')
+      {
+        result.push(array[i])
+      }
+      else
+      {
+        reee.push(array[i])
+      }
+    }
+    else
+    {
+      result.push(array[i])
+    }
+  }
+
+  let number = []
+  let bool = []
+  let string = []
+
+  let hasil = []
+  
+  if (result.length === 0)
+  {
+    return []
+  }
+  else
+  {
+    for (let i = 0; i < result.length; i++)
+  {
+    if (typeof result[i] === 'number')
+    {
+      number.push(result[i])
+    } 
+    else if (typeof result[i] === 'string')
+    {
+      string.push(result[i])
+    }
+    else if (typeof result[i] === 'boolean')
+    {
+      bool.push(result[i])
+    } 
+  }
+  }
+
+  hasil.push(number)
+  hasil.push(string)
+  hasil.push(bool)
+
+  hasil[0].sort()
+  hasil[2].sort()
+
+  return hasil
+}
+
+console.log(sortAllClean([ undefined, null, 456, 'def', NaN, [], true, 123, 'bcd', false]));
+//[ [ 123, 456 ], [ 'bcd', 'def' ], [ false, true ] ]
+
+console.log(sortAllClean([ NaN, undefined ])); // []
